d43f3fbe2788a65750442a88c2b68e1bebcec413
tangcong <1432339230@qq.com>
2025-03-21 19:59:25 +0800

    feat: (新增页面)

---
+++ src/pages/chooseDiners/index.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+  <view>
+    <text>选位置</text>
+  </view>
+</template>
+<style lang="scss" scoped></style>
--- src/pages/foodorder/index.vue
+++ src/pages/foodorder/index.vue
@@ -1,20 +1,46 @@
 <script setup>
+  import { reactive } from "vue";
   import { onLoad } from "@dcloudio/uni-app";
   import { queryTableFoodOrderApi } from "@/api/restaurant";
   import { useBaseStore } from "@/pinia";
   const baseInfo = useBaseStore();
+  //当前扫码进来的桌子信息
+  const currentTableInfo = reactive({
+    mid: "",
+    tno: "",
+    foodOrderId: "",
+    consumerCount: 0
+  });
+
   onLoad(async (options) => {
+    uni.showLoading();
+    currentTableInfo.mid = options.mid;
+    currentTableInfo.tno = options.tno;
+    currentTableInfo.foodOrderId = options.foodOrderId;
+    currentTableInfo.consumerCount = parseInt(options.personNum, 10);
+    await initData();
+  });
+
+  const initData = async () => {
     try {
       const res = await queryTableFoodOrderApi(
         baseInfo.baseBody,
-        options?.mid,
-        options?.tno
+        currentTableInfo?.mid,
+        currentTableInfo?.tno
       );
-      console.log(res, "---dasd---dasdasdasdad");
+      if (res.ret_code === "0000") {
+        // 请求成功
+      } else if (res.ret_code === "3100") {
+        // 桌子被清理了（跳转到选择人数的页面）
+        uni.reLaunch({
+          url: `/pages/chooseDiners/index?mid=${currentTableInfo.mid}&tno=${currentTableInfo.tno}`
+        });
+      }
+      uni.hideLoading();
     } catch (error) {
       console.error(error);
     }
-  });
+  };
 </script>
 <template>
   <view>dsaf</view>
